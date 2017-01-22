07d89e750c1ea146bd7458d0c521e4b2b332debd
Nicky Parseghian <atchoum2k@gmail.com>
2017-01-22 12:36:11 -0800

    Fix invalid Threadsafe variable in app.yaml Cleaning main.app

--- appEngine/main.py
+++ appEngine/main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: givemelove
 # @Date:   2016-11-30 09:56:10
-# @Last Modified by:   givemelove
-# @Last Modified time: 2016-11-30 11:07:48
+# @Last Modified by:   Nicky Parseghian
+# @Last Modified time: 2017-01-22 12:35:57
 
 import webapp2
 import share
@@ -10,31 +10,8 @@ import logging
 import zlib
 
 class MainHandler(webapp2.RequestHandler):
-	def delete(self):
-		self.response.set_status(200)
-		self.response.out.write(' ')
-
 	def get(self):
-		# logging.info(self.request.url)
-		# logging.info(self.request.query_string)
-		# logging.info(self.request.headers)
-		# logging.info(self.request.cookies)
-
-		self.response.out.write('{ "id": "com.google.enterprise.springboard.glossary.Glossary||A: Glossary", "readers": [ { "kind": "customer" } ], "createdTime": "2016-11-30T18:07:11.233Z", "itemUpdatedTime": "2016-11-30T18:07:11.672Z"}')
-
-	def put(self):
-		logging.info('--- Receiving a PUT request ---')
-		logging.info('')
-		logging.info('')
-		logging.info('')
-		logging.info('')
-		logging.info(self.request.url)
-		# logging.info(self.request.query_string)
-		logging.info(self.request.headers)
-		# logging.info(self.request.cookies)
-		logging.info(zlib.decompress(self.request.body, 16+zlib.MAX_WBITS))
-
-
+		pass
 
 app = webapp2.WSGIApplication([('.*', MainHandler)],
                               debug=share.wsgiDebug)

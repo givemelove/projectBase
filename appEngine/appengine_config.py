5635969d98b26f2c1a51fe67bec290ea650cdf36
Nicky Parseghian <atchoum2k@gmail.com>
2017-01-22 14:14:04 -0800

    Add appengine_config.py to include libraries

---
+++ appEngine/appengine_config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Nicky Parseghian
+# @Date:   2017-01-22 14:12:41
+# @Last Modified by:   Nicky Parseghian
+# @Last Modified time: 2017-01-22 14:12:44
+
+from google.appengine.ext import vendor
+
+# Add any libraries install in the "lib" folder.
+vendor.add('lib')
